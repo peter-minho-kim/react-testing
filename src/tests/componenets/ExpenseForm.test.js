f71b157ec72bd0ba74fb113ba851a8555154ab5c
Peter Kim <peter.minho.kim@gmail.com>
2020-08-29 12:16:02 -0400

    Add test spies

--- src/tests/componenets/ExpenseForm.test.js
+++ src/tests/componenets/ExpenseForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
@@ -16,7 +17,7 @@ test('should render ExpenseForm correctly with expense data', () => {
 test('should render error for invalid form submission', () => {
   const wrapper = shallow(<ExpenseForm />);
   expect(wrapper).toMatchSnapshot();
-  wrapper.find('form').simulate('submit', { preventDefault: () => {} }); // simulate e (event)
+  wrapper.find('form').simulate('submit', { preventDefault: () => {} }); // simulate e (event) and simulate form submission
   expect(wrapper.state('error').length).toBeGreaterThan(0);
   expect(wrapper).toMatchSnapshot();
 });
@@ -56,4 +57,33 @@ test('should not set amount if invalid input', () => {
     target: { value }
   });
   expect(wrapper.state('amount')).toBe('');
+});
+
+test('should call onSubmit prop for valid form sumbmission', () => {
+  const onSubmitSpy = jest.fn();
+  const wrapper = shallow(
+    <ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />
+  );
+  wrapper.find('form').simulate('submit', { preventDefault: () => {} }); // simulate e (event)
+  expect(wrapper.state('error')).toBe('');
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    note: expenses[0].note,
+    createdAt: expenses[0].createdAt
+  });
+});
+
+test('should set new date on date change', () => {
+  const now = moment();
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('SingleDatePicker').prop('onDateChange')(now);
+  expect(wrapper.state('createdAt')).toEqual(now);
+});
+
+test('should set calendar focus on change', () => {
+  const focused = true;
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('SingleDatePicker').prop('onFocusChange')({ focused });
+  expect(wrapper.state('calendarFocused')).toBe(focused);
 });
